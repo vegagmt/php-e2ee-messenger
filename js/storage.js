ff04e69b94d74a6fb6a6baeef087402929071f4a
Onowe Vega <support@nexoic.com>
2026-05-30 18:54:20 +0400

    Update storage.js
    Js gen key up

--- js/storage.js
+++ js/storage.js
@@ -1,78 +1,62 @@
-// js/storage.js
-
 const KeyStorage = {
-     
-    PUB_KEY_KEY: 'e2ee_public_key',
-    PRIV_KEY_KEY: 'e2ee_private_key_enc',
+    PUB_KEY_KEY:      'e2ee_pub',
+    PRIV_KEY_KEY_ENC: 'e2ee_priv_enc',
+    STORAGE_KEY_KEY:  'e2ee_storage_key', 
 
  
-    _getStorageKey() {
-        if (!window.APP_SECRET) {
-            throw new Error("Critical error: APP_SECRET is not defined on the page!");
+    _getOrCreateStorageKey() {
+        const existing = localStorage.getItem(this.STORAGE_KEY_KEY);
+        if (existing) {
+            return sodium.from_base64(existing, sodium.base64_variants.URLSAFE_NO_PADDING);
         }
-        
-        return sodium.crypto_generichash(
-            sodium.crypto_secretbox_KEYBYTES, 
-            sodium.from_string(window.APP_SECRET)
+         
+        const newKey = sodium.randombytes_buf(sodium.crypto_secretbox_KEYBYTES);
+        localStorage.setItem(
+            this.STORAGE_KEY_KEY,
+            sodium.to_base64(newKey, sodium.base64_variants.URLSAFE_NO_PADDING)
         );
+        return newKey;
     },
 
- 
     hasKeys() {
-        return !!localStorage.getItem(this.PUB_KEY_KEY) && !!localStorage.getItem(this.PRIV_KEY_KEY);
+        return !!localStorage.getItem(this.PUB_KEY_KEY) &&
+               !!localStorage.getItem(this.PRIV_KEY_KEY_ENC);
     },
 
- 
     saveKeys(publicKey, privateKey) {
-        const storageKey = this._getStorageKey();
-
-        // 1. Saving the public key in base64url
-        const pubKeyStr = sodium.to_base64(publicKey, sodium.base64_variants.URLSAFE_NO_PADDING);
-        localStorage.setItem(this.PUB_KEY_KEY, pubKeyStr);
-
-        // 2. Encrypting the private key
-        const nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
-        
-        // Encrypting: (privateKey, nonce, storageKey)
+        const storageKey = this._getOrCreateStorageKey();
+        const nonce      = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
         const ciphertext = sodium.crypto_secretbox_easy(privateKey, nonce, storageKey);
 
-        // 3. Packing nonce and ciphertext into JSON for easy storage
-        const encryptedBundle = {
-            nonce: sodium.to_base64(nonce, sodium.base64_variants.URLSAFE_NO_PADDING),
+        localStorage.setItem(this.PUB_KEY_KEY,
+            sodium.to_base64(publicKey, sodium.base64_variants.URLSAFE_NO_PADDING));
+        localStorage.setItem(this.PRIV_KEY_KEY_ENC, JSON.stringify({
+            nonce:      sodium.to_base64(nonce,      sodium.base64_variants.URLSAFE_NO_PADDING),
             ciphertext: sodium.to_base64(ciphertext, sodium.base64_variants.URLSAFE_NO_PADDING)
-        };
-
-        localStorage.setItem(this.PRIV_KEY_KEY, JSON.stringify(encryptedBundle));
+        }));
     },
- 
+
     loadKeys() {
         if (!this.hasKeys()) return null;
-
         try {
-            const pubKeyStr = localStorage.getItem(this.PUB_KEY_KEY);
-            const encryptedBundleStr = localStorage.getItem(this.PRIV_KEY_KEY);
-
-            const bundle = JSON.parse(encryptedBundleStr);
-            const storageKey = this._getStorageKey();
-
-         
-            const publicKey = sodium.from_base64(pubKeyStr, sodium.base64_variants.URLSAFE_NO_PADDING);
-            const nonce = sodium.from_base64(bundle.nonce, sodium.base64_variants.URLSAFE_NO_PADDING);
-            const ciphertext = sodium.from_base64(bundle.ciphertext, sodium.base64_variants.URLSAFE_NO_PADDING);           
-            const privateKey = sodium.crypto_secretbox_open_easy(ciphertext, nonce, storageKey);
-
-            return { publicKey, privateKey };
-        } catch (error) {
-            console.error("Error reading or decrypting keys. The session may have changed.", error);
-           
-            this.clearKeys();
-            return null;
+            const storageKey = this._getOrCreateStorageKey();
+            const pubKey     = sodium.from_base64(
+                localStorage.getItem(this.PUB_KEY_KEY),
+                sodium.base64_variants.URLSAFE_NO_PADDING
+            );
+            const bundle     = JSON.parse(localStorage.getItem(this.PRIV_KEY_KEY_ENC));
+            const nonce      = sodium.from_base64(bundle.nonce,      sodium.base64_variants.URLSAFE_NO_PADDING);
+            const ciphertext = sodium.from_base64(bundle.ciphertext, sodium.base64_variants.URLSAFE_NO_PADDING);
+            const privKey    = sodium.crypto_secretbox_open_easy(ciphertext, nonce, storageKey);
+            return { publicKey: pubKey, privateKey: privKey };
+        } catch (e) {
+            console.error('Error loading keys:', e);
+            return null; 
         }
     },
 
- 
     clearKeys() {
-        localStorage.removeItem(this.PUB_KEY_KEY);
-        localStorage.removeItem(this.PRIV_KEY_KEY);
+        [this.PUB_KEY_KEY, this.PRIV_KEY_KEY_ENC, this.STORAGE_KEY_KEY]
+            .forEach(k => localStorage.removeItem(k));
     }
 };
